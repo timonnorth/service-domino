8ee1a082a6d8e3f64d617541b11d6424153da98d
Oleksandr Ieremeev <yeremeyev.alexander@gmail.com>
2020-07-24 01:32:34 +0200

    api tests

--- tests/Controller/JsonRpc/ApiTest.php
+++ tests/Controller/JsonRpc/ApiTest.php
@@ -5,16 +5,43 @@ declare(strict_types=1);
 namespace Tests\Controller\JsonRpc;
 
 use Controller\JsonRpc\Api;
+use Controller\JsonRpc\ArgumentException;
 use Tests\TestCase;
 
 class ApiTest extends TestCase
 {
-    public function testAdd()
+    /** @var string */
+    protected $gameId;
+    /** @var string */
+    protected $playerId;
+    /** @var string */
+    protected $playerSecret;
+
+    public function testCreateGameAndGetMatch()
     {
         $api = new Api($this->getContainer());
         $res = $api->evaluate("new-match", ['rules' => 'basic', 'name' => 'Tiesto', 'players' => 2]);
         self::assertIsArray($res);
         self::assertTrue($res['id'] != '');
         self::assertEquals('Tiesto', $res['player']['name']);
+
+        $api->evaluate("get-match", ['gameId' => $res['id'], 'playerId' => $res['player']['id'], 'playerSecret' => $res['player']['secret']]);
+    }
+
+    public function testGetRules()
+    {
+        $api = new Api($this->getContainer());
+        $res = $api->evaluate("rules", []);
+        self::assertIsArray($res);
+        self::assertEquals(['basic', 'kozel', 'traditional'], $res);
+    }
+
+    public function testGetMatchValidation()
+    {
+        $api = new Api($this->getContainer());
+        $this->expectException(ArgumentException::class);
+        $this->expectExceptionMessage('Param "gameId" is undefined');
+        $api->evaluate("get-match", []);
+
     }
 }
--- tests/Service/GameBigMatchTest.php
+++ tests/Service/GameBigMatchTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Tests\Service;
 
-use Entity\Match;
 use Service\Game;
 use Service\GameFactory;
 use Tests\TestCase;
@@ -24,12 +23,12 @@ class GameBigMatchTest extends TestCase
         /** @var Serializer $serializer */
         $serializer = $this->getContainer()->get('Serializer');
 
-        $players = $game->getMatch()->players;
-        $players[0]->marker = false;
-        $players[1]->marker = true;
-        $players[2]->marker = false;
-        $players[3]->marker = false;
-        $game->getMatch()->events = [];
+        $players                              = $game->getMatch()->players;
+        $players[0]->marker                   = false;
+        $players[1]->marker                   = true;
+        $players[2]->marker                   = false;
+        $players[3]->marker                   = false;
+        $game->getMatch()->events             = [];
         $game->getMatch()->stock->tiles->list = [];
         // Draw new tiles.
         $players[0]->tiles->list = [Tile::create(0, 0), Tile::create(1, 2)];
@@ -51,46 +50,38 @@ class GameBigMatchTest extends TestCase
         $events = $game->getMatch()->events;
         self::assertEquals(6, count($events));
         // Event 0.
-        $tile = Tile::create(2, 2);
+        $tile              = Tile::create(2, 2);
         $tile->orientation = $events[0]->data->tile->orientation; // We do not control orientation of first Tile.
-        $event = Event::create('play', DataPlay::create($tile, null, 'root'), $players[1]->id);
-        $event->createdAt = $events[0]->createdAt;
+        $event             = Event::create('play', DataPlay::create($tile, null, 'root'), $players[1]->id);
+        $event->createdAt  = $events[0]->createdAt;
         self::assertJsonStringEqualsJsonString($serializer->serialize($event), $serializer->serialize($events[0]));
         $tileFirst = $tile;
         // Event 1.
-        $event = Event::create('skip', null, $players[2]->id);
+        $event            = Event::create('skip', null, $players[2]->id);
         $event->createdAt = $events[1]->createdAt;
         self::assertJsonStringEqualsJsonString($serializer->serialize($event), $serializer->serialize($events[1]));
         // Event 2.
-        $event = Event::create('skip', null, $players[3]->id);
+        $event            = Event::create('skip', null, $players[3]->id);
         $event->createdAt = $events[2]->createdAt;
         self::assertJsonStringEqualsJsonString($serializer->serialize($event), $serializer->serialize($events[2]));
         // Event 3.
-        $tileParent = $tile;
-        $tile = Tile::create(1, 2);
+        $tileParent        = $tile;
+        $tile              = Tile::create(1, 2);
         $tile->orientation = 0;
-        $event = Event::create('play', DataPlay::create($tile, $tileParent, 'left'), $players[0]->id);
-        $event->createdAt = $events[3]->createdAt;
+        $event             = Event::create('play', DataPlay::create($tile, $tileParent, 'left'), $players[0]->id);
+        $event->createdAt  = $events[3]->createdAt;
         self::assertJsonStringEqualsJsonString($serializer->serialize($event), $serializer->serialize($events[3]));
         // Event 4.
-        $tileParent = $tileFirst;
-        $tile = Tile::create(2, 3);
+        $tileParent        = $tileFirst;
+        $tile              = Tile::create(2, 3);
         $tile->orientation = 0;
-        $event = Event::create('play', DataPlay::create($tile, $tileParent, 'right'), $players[1]->id);
-        $event->createdAt = $events[4]->createdAt;
+        $event             = Event::create('play', DataPlay::create($tile, $tileParent, 'right'), $players[1]->id);
+        $event->createdAt  = $events[4]->createdAt;
         self::assertJsonStringEqualsJsonString($serializer->serialize($event), $serializer->serialize($events[4]));
         // Event 5.
-        $event = Event::create('win', Event\DataScore::create(5, (3+4)+(3+3)+(4+5)+(4+4)+25), $players[1]->id);
+        $event            = Event::create('win', Event\DataScore::create(5, (3 + 4) + (3 + 3) + (4 + 5) + (4 + 4) + 25), $players[1]->id);
         $event->createdAt = $events[5]->createdAt;
         self::assertJsonStringEqualsJsonString($serializer->serialize($event), $serializer->serialize($events[5]));
-
-
-        //$method = new \ReflectionMethod(Game::class, 'doSomethingFancy');
-        //$method->setAccessible(true);
-        //$result = $game->play(Tile::create(2, 3), 'root', $game->getMatch()->players[1]->id);
-        //self::assertFalse($result->hasError());
-        //var_dump($result->getError());
-        var_dump($game->getMatch());
     }
 
     protected function startNewMatch($rules = 'kozel'): Game
